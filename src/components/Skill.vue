83548c361bf684335a56501b553bc1ca19799bf7
minthukha99 <minthukha99@gmail.com>
2022-10-04 17:05:48 +0800

    html addition to skills

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,17 +1,55 @@
 <template>
-    <div class="skillList">
-      <h1>Skills</h1>
-    </div>
+  <div class="skillList">
+    <h1>Skills</h1>
+    <input type="button" style="float:right" value="+Add skill">
+  </div>
+  <br>
+  <div>
+    <table>
+      <tr>
+        <td>
+          1.
+        </td>
+        <th>
+          Skill123
+        </th>
+        <td>
+          Edit
+        </td>
+        <td>
+          Activate
+        </td>
+        <td>
+          Deactivate
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
   
 <script>
-    export default {
-    name: 'Skills',
-    }
+export default {
+  name: 'Skills',
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+table {
+  border-collapse: collapse;
+  width: 70%
+}
 
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+
+}
+
+td {
+  border: 1px solid #dddddd;
+  width: 20px;
+  padding: 8px;
+}
 </style>
-  
