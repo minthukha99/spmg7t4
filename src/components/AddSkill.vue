9f4dea4edb29ab4e593642cffe550ac85b458b9e
yeekhai <yeekhai12@gmail.com>
2022-10-06 19:36:18 +0800

    added create for skills and roles

--- src/components/AddSkill.vue
+++ src/components/AddSkill.vue
@@ -4,21 +4,95 @@
             <h1>Add Skill</h1>
             <form>
                 <label for="skillName">Name of the Skill</label><br>
-                <input type="text" id="skillName" name="skillName"><br>
+                <input v-model="skillName" id="skillName" name="skillName"><br>
+                <br>
+                <label for="skillsNeeded" class="multiselect" >Roles that require skill</label>
+                <div class="selectBox">
+                    <select multiple v-model="selectedRoles">
+                        <option selected="true" disabled="disabled">Select an option (if any)</option>
+                        <option v-for="role in rolesList" :key="role.id">>{{role.roleName}}</option>       
+                    </select>
+                </div>
+                <br>
                 <br>
                 <button value="Cancel" class="special">
                     <router-link to="/Skills" class="special">Cancel</router-link>
-                    
                 </button> 
-                <button type="submit" value="Save" class="special">Save</button>  
+                <button @click='createSkill()' type="submit" value="Save" class="special">
+                    <router-link to="/Skills" class="special">Save</router-link>
+                </button>  
             </form>
         </div>
     </div>
 </template>
   
 <script>
+import axios from "axios";
+
 export default {
-    name: 'Add Skill',
+  name: 'Roles',
+
+  mounted() {
+    this.getRoles()
+  },
+
+  data() {
+    return {
+      rolesList: [],
+      selectedRoles: [],
+      skillName: ""
+    }
+  },
+
+    methods: {
+        getRoles() {
+            const url = "http://localhost:3000/roles";
+            axios.get(url)
+            .then(response => {
+                var roleData = response.data
+                // console.log("roleData=", roleData)
+                for (var role of roleData) {
+                this.rolesList.push(
+                    {
+                    id: role._id,
+                    roleName: role.roleName,
+                    skillName: role.skillName,
+                    status: role.status
+                    }
+                );
+                }
+                // console.log("rolesList=", this.rolesList)
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+        },
+
+
+        createSkill(){
+
+            let url = "http://localhost:3000/skill";
+            axios.post(url, {
+                skillName: this.skillName,
+                roleName: this.selectedRoles
+            })
+
+            .then(response => {
+                console.log("new role:", this.skillName, this.selectedRoles)
+                location.reload()
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+
+
+        }
+
+
+
+
+
+    }
 }
 </script>
 
