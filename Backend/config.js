30f63d5c037d7cb9b6a10187bc33a189f65abd43
TianFengScis <89022439+TianFengScis@users.noreply.github.com>
2022-10-11 18:08:11 +0800

    AWS databast setup
    Alter all current apis to connect to RDBMS -Mysql

---
+++ Backend/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: "spmg7t4-db.cijmad217w0e.us-west-2.rds.amazonaws.com",
+        user: "admin",
+        password: "tKw2DK1vxFN1NzhEYqT0",
+        database: "spm"
+    },
+    listperPage: 10,
+};
+module.exports = config;
---
+++ Backend/db.js
@@ -0,0 +1,15 @@
+const { connect } = require('mongoose');
+const mysql = require('mysql2/promise');
+const config = require('./config');
+
+async function query(sql, params) {
+    const connection = await mysql.createConnection(config.db);
+    const [results,] = await connection.execute(sql, params).catch(error => {
+        console.log(error);
+    });
+    return results;
+}
+
+module.exports = {
+    query
+}
