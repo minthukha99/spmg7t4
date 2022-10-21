c7def1f45927ee42ab17690b9cec18b0c243fb37
TianFengScis <89022439+TianFengScis@users.noreply.github.com>
2022-10-21 14:07:34 +0800

    Changed DB region

--- Backend/config.js
+++ Backend/config.js
@@ -1,6 +1,6 @@
 const config = {
     db: {
-        host: "spmg7t4-db.cijmad217w0e.us-west-2.rds.amazonaws.com",
+        host: "spmg7t4-sgdb.cm5iarqpvubq.ap-southeast-1.rds.amazonaws.com",
         user: "admin",
         password: "tKw2DK1vxFN1NzhEYqT0",
         database: "spm"
