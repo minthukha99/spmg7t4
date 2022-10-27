671351f5a13519ebc5416ba679647b7cb342b320
minthukha99 <minthukha99@gmail.com>
2022-10-28 01:58:04 +0800

    deleted comments in config.js

--- Backend/config.js
+++ Backend/config.js
@@ -20,20 +20,4 @@ const config = {
     listperPage: 10,
 };
 module.exports = config;
-// const config = {
-//     db: {
-//         // host: "spmg7t4.cm5iarqpvubq.ap-southeast-1.rds.amazonaws.com",
-//         // user: "admin",
-//         // password: "$nGMccUPuF^i002*vS$W",
 
-//         host: "localhost",
-//         port: 8889,
-//         user: "root",
-//         password: "root",
-
-//         database: "spm"
-        
-//     },
-//     listperPage: 10,
-// };
-// module.exports = config;
