26c6c51ab7044554add35b674ebcd97c754985e3
asianburpgirl <estherrrlychee@gmail.com>
2022-10-29 22:16:54 +0800

    editing class names for unit test

--- src/components/UpdateSkill.vue
+++ src/components/UpdateSkill.vue
@@ -5,7 +5,7 @@
 
         <form>
             <label for="skillName">Skill Name</label><br>
-            <input v-model="newSkillName" id="skillName" name="skillName"><br>
+            <input v-model="newSkillName" class="skillName" name="skillName"><br>
             <br>
             <label for="skillsNeeded" class="multiselect">Roles that require skill</label>
             <br>
@@ -15,10 +15,10 @@
             </select>
             <br>
             <br>
-            <button value="Cancel" class="special">
+            <button value="Cancel" class="cancelButton">
                 <router-link to="/Skills" class="special">Cancel</router-link>
             </button>
-            <button @click='updateSkill(skillName); $router.push("/Skills")' value="Save" class="special">
+            <button @click='updateSkill(skillName); $router.push("/Skills")' name="Save" class="special saveButton">
                 Save
             </button>
         </form>
@@ -30,7 +30,7 @@
 import axios from "axios";
 export default {
 
-    name: 'UpdateSkill/:id',
+    name: 'UpdateSkill',
 
     mounted() {
         this.getRoles()
@@ -72,12 +72,12 @@ export default {
         },
 
         updateSkill() {
-            console.log(skillName)
+            // console.log(skillName)
 
             let url = "http://localhost:3000/updateskill/" + this.id;
-            console.log(url)
-
-            console.log(this.newSkillName)
+            // console.log(url)
+            // console.log(typeof(this.id))
+            // console.log(this.newSkillName)
 
             axios.put(url, {
                     roleName: this.selectedRoles,
---
+++ tests/unit/UpdateSkill.spec.js
@@ -0,0 +1,27 @@
+// import { shallowMount } from '@vue/test-utils'
+// import 
+// UpdateSkill from '@/components/UpdateSkill.vue'
+
+// describe('UpdateSkill.vue', () => {
+//   test('submits updated details of a skill', async() => {
+//     const wrapper = shallowMount(UpdateSkill, {
+//         props: {
+//             newSkillName: 'skill5'
+//         }
+//     })
+    
+//     await wrapper.find('.skillName').setValue('skill1')
+//     await wrapper.find('select').setValue('RoleUpdateWorks')
+
+//     expect(wrapper.props('newSkillName')).toBe('skill1')
+
+//     await wrapper.find('.cancelButton').trigger('click')
+//     await wrapper.find('.saveButton').trigger('click')
+
+
+//   })
+// })
+
+
+
+
